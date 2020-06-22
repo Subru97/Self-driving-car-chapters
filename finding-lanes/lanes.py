bbfadc87d109f9c2ec505f7f898276fa227476d5
Subramanian <subru1997@gmail.com>
2020-06-22 10:56:37 +0200

    detect lane

--- finding-lanes/lanes.py
+++ finding-lanes/lanes.py
@@ -2,23 +2,77 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+def make_coordinates(images, line_parameters):
+    try:
+        slope, intercept = line_parameters
+    except:
+        slope, intercept = 0.001, 0
+    y1 = images.shape[0]
+    y2 = int(y1*(3/5))
+    x1 = int((y1 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope)
+    return np.array([x1, y1, x2, y2])    
+
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        parameters = np.polyfit((x1,x2), (y1,y2), 1)
+        slope = parameters[0]
+        intercept = parameters[1]
+        if slope < 0:
+            left_fit.append((slope, intercept))
+        else:
+            right_fit.append((slope, intercept))
+
+    left_fit_avg = np.average(left_fit, axis=0)
+    right_fit_avg = np.average(right_fit, axis=0)
+    left_line = make_coordinates(image, left_fit_avg)
+    right_line = make_coordinates(image, right_fit_avg)
+    return np.array([left_line, right_line])
+
 def canny(image):
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray, (5,5), 0)
     canny = cv2.Canny(blur, 50, 150)
     return canny
 
+def display_lines(image, lines):
+    lines_image = np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(lines_image, (x1,y1), (x2,y2), (255, 0, 0), 10)
+    return lines_image
+    
 def region_of_interest(image):
     height = image.shape[0]
     polygons = np.array([[(200, height), (1100, height), (550, 250)]])
     mask = np.zeros_like(image)
-    cv2.fillPoly(mask, polygons, 255)
+    cv2.fillPoly(mask, np.array([polygons], dtype=np.int32), 255)
     masked_image = cv2.bitwise_and(image, mask)
     return masked_image
     
-image = cv2.imread('test_image.jpg')
-lane_image = np.copy(image)
-canny = canny(lane_image)
-roi = region_of_interest(canny)
-cv2.imshow('result', roi)
+# image = cv2.imread('test_image.jpg')
+# lane_image = np.copy(image)
+# canny_image = canny(lane_image)
+# roi = region_of_interest(canny_image)
+# lines = cv2.HoughLinesP(roi, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+# averaged_lines = average_slope_intercept(lane_image, lines)
+# lines_image = display_lines(lane_image, averaged_lines)
+# lane_marked_image = cv2.addWeighted(lane_image, 0.8, lines_image, 1, 1)
+# cv2.imshow('result', lane_marked_image)
 # plt.imshow(canny)
+
+video_capture = cv2.VideoCapture('test2.mp4')
+while(video_capture.isOpened()):
+    _, frame = video_capture.read()
+    canny_image = canny(frame)
+    roi = region_of_interest(canny_image)
+    lines = cv2.HoughLinesP(roi, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+    averaged_lines = average_slope_intercept(frame, lines)
+    lines_image = display_lines(frame, averaged_lines)
+    lane_marked_image = cv2.addWeighted(frame, 0.8, lines_image, 1, 1)
+    cv2.imshow('result', lane_marked_image)
+    cv2.waitKey(1)
